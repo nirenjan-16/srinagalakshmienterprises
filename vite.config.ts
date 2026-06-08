a9f22f055441ba2a9aceb0780b4da13ffdb333b7
nirenjan-16 <162408464+nirenjan-16@users.noreply.github.com>
2026-06-08 13:23:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  tanstackStart: {
+    server: { entry: "server" },
+  },
 });
